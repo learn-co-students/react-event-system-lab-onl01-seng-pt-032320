188e66aaed4fecea188a6b5ce26d185b0bd914dd
Shamel Lakin <christlife4u@gmail.com>
2020-11-30 16:57:49 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,36 @@
 // Code EyesOnMe Component Here
+import React, { Component} from 'react';
+
+export default class EyesOnMe extends React.Component{
+
+    constructor(){
+        super()
+    }
+
+    onFocus = (event) => {
+        console.log('Good!');
+    }
+
+    onBlur = (event) => {
+        console.log('Hey! Eyes on me!');
+    }
+
+    render(){
+        return (
+        <button onFocus={this.onFocus} onBlur={this.onBlur}>focus</button>
+        )
+    }
+}
+
+{/* <Keypad />
+✓ should have one input
+✓ should have the right input type
+Entering password...
+✓ should trigger console output when typing (42ms)
+
+<EyesOnMe />
+✓ should have one button
+Good!
+✓ should trigger console output when focusing
+Hey! Eyes on me!
+✓ should trigger console output when removing focus (blur) */}
